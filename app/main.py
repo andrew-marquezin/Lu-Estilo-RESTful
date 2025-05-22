ea9445cb3b864d8761cc125fe7ec91438e1ec3d7
Andrew Marquezin Marinho <andrewmmarinho@gmail.com>
2025-05-22 18:36:36 -0300

    conexão com o db

--- app/main.py
+++ app/main.py
@@ -1,14 +1,32 @@
-from typing import Union
-from fastapi import FastAPI
+from typing import Annotated
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI, Depends
+from sqlmodel import Session, SQLModel
+from db import engine
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+SessionDep = Annotated[Session, Depends(get_session)]
 
 app = FastAPI()
+
+
+@asynccontextmanager
+async def lifespan(app):
+    create_db_and_tables()
+    yield
+    # Cleanup code
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
